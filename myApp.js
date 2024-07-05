4158614b8a4dde4ae5527e1101dbb7496e442dd6
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-05 15:12:01 +0300

    solve "Serve an HTML File"

--- myApp.js
+++ myApp.js
@@ -5,7 +5,8 @@ let app = express();
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  // res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 
