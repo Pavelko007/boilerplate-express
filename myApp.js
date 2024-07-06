2ae20aec6a1884a95eb16fdc0a52b5a36e10f93d
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-06 12:13:40 +0300

    solve "Chain Middleware to Create a Time Server"

--- myApp.js
+++ myApp.js
@@ -1,30 +1,38 @@
 let express = require('express');
 let app = express();
 let dotenv = require('dotenv');
+const { json } = require('body-parser');
 
 // load the environment variables
-dotenv.config();
+// dotenv.config();
 
 // console.log("Hello World");
 
-app.use((req, res, next) =>{
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-})
+// app.use((req, res, next) =>{
+//   console.log(`${req.method} ${req.path} - ${req.ip}`);
+//   next();
+// })
 
-app.use('/public', express.static(__dirname + '/public'))
+// app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-  // res.send('Hello Express');
-  res.sendFile(__dirname + '/views/index.html');
-})
+// app.get('/', (req, res) => {
+//   // res.send('Hello Express');
+//   res.sendFile(__dirname + '/views/index.html');
+// })
 
-app.get('/json', (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === 'uppercase'){
-    message = message.toUpperCase();
-  }
-  res.json({"message": message})
+// app.get('/json', (req, res) => {
+//   let message = "Hello json";
+//   if (process.env.MESSAGE_STYLE === 'uppercase'){
+//     message = message.toUpperCase();
+//   }
+//   res.json({"message": message})
+// })
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next()
+}, (req, res) => {
+  res.json({time: req.time})
 })
 
 
